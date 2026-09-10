8aca989e2ad72732022083847f8dd6548f24bd56
yaso <yashnov.dev@gmail.com>
2026-09-10 15:12:06 +0300

    I hope that is OK

--- JavaRush/src/level4/lecture8/task_3/Person.java
+++ JavaRush/src/level4/lecture8/task_3/Person.java
@@ -1,4 +1,10 @@
 package level4.lecture8.task_3;
 
-public class Person {
+public interface Person {
+    class User implements Person {
+        void live() {
+            System.out.println("I usually just live.");
+        }
+    }
+
 }
